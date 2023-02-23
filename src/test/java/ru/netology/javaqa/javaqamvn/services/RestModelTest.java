63550e1efdfe13f56997036ae9640be9ca35497c
Andrey Kucherenko <andykucherenko@gmail.com>
2023-02-23 20:13:46 +0300

    Parameterized Test

--- src/test/java/ru/netology/javaqa/javaqamvn/services/RestModelTest.java
+++ src/test/java/ru/netology/javaqa/javaqamvn/services/RestModelTest.java
@@ -1,24 +1,26 @@
 package ru.netology.javaqa.javaqamvn.services;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class RestModelTest {
-    @Test
-        public void shouldCalculateMonthsFirstYear() {
-
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/rest.csv")
+    public void shouldCalculateMonthsFirstYear(int income, int threshold, int expences, long expected) {
         RestModel service = new RestModel();
 
-        int income = 10_000;
-        int threshold = 20_000;
-        int expences = 3_000;
-        long expected = 3;
+        //int income = 10_000;
+        //int threshold = 20_000;
+        //int expences = 3_000;
+        //long expected = 3;
 
         int actual = service.calculate(income, expences, threshold);
 
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
+    /*@Test
     public void shouldCalculateMonthsSecondYear() {
 
         RestModel service = new RestModel();
@@ -31,5 +33,5 @@ public class RestModelTest {
         int actual = service.calculate(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
-    }
+    }*/
 }
